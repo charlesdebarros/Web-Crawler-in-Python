3a1aa98139fffe14eb676968cbc3aa1083503fcf
Charles De Barros <charlesdebarros@yahoo.co.uk>
2016-07-05 22:01:55 +0100

    Defining methods to creating and writing data files

--- general.py
+++ general.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+import os
+
 # Each website crawled has its own project folder
 
-import os
 
 def create_project_dir(directory):
     if not os.path.exists(directory):
@@ -10,3 +11,22 @@ def create_project_dir(directory):
         os.makedirs(directory)
 
 create_project_dir('testdirectory')
+
+
+# Create queue and crawled files (if not created)
+
+def create_data_files(project_name, base_url):
+    queue = project_name + 'queue.txt'
+    crawled = project_name + 'crawled.txt'
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+
+# Create a new file
+
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
