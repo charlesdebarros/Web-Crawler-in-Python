8f57e930a81e5b9f6d7e818412fa4803b35cbf3a
Charles De Barros <charlesdebarros@yahoo.co.uk>
2016-07-14 21:18:19 +0100

    Adding html.parser module

--- link_finder.py
+++ link_finder.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 3.3.0
 
-from html.parser import HTMLparser
+from html.parser import HTMLParser
 from urllib import parse
 
 
@@ -10,7 +10,12 @@ class LinkFinder(HTMLParser):
         super().__init__()
 
     def handle_starttag(self, tag, attrs):
-        pass
+        print(tag)
 
     def error(self, message):
         pass
+
+
+finder = LinkFinder()
+finder.feed('<html><head><title>Test</title></head>'
+            '<body><h1>Parse me!</h1></body></html>')
