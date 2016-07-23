a142ca0cfd15f0b0189f81d1efef26fbf683e20a
Charles De Barros <charlesdebarros@yahoo.co.uk>
2016-07-23 17:39:06 +0100

    Setting Class variables for the Spider Class

--- spider.py
+++ spider.py
@@ -7,4 +7,14 @@ from general import *
 
 class Spider:
 
+    # Class variables are shared among all instances
+    project_name = ''
+    base_url = ''
+    domain_name = ''
+    queue_file = ''
+    crawled_file = ''
+    queue = set()
+    crawled = set()
+
     def __init__(self):
+        pass
