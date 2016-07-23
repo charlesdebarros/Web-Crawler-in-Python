079701a188da98f7bf96e3e4a336dcdf68f93f92
Charles De Barros <charlesdebarros@yahoo.co.uk>
2016-07-23 17:53:32 +0100

    Setting Spider class __init__() method.

--- spider.py
+++ spider.py
@@ -16,5 +16,11 @@ class Spider:
     queue = set()
     crawled = set()
 
-    def __init__(self):
-        pass
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
+        self.boot()
+        self.crawl_page('Spider #1', Spider.base_url)
