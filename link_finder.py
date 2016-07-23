4a82d7fc67a098a6c31dc82d17656a919182c62b
Charles De Barros <charlesdebarros@yahoo.co.uk>
2016-07-23 16:51:14 +0100

    Completing LinkFinder class

--- link_finder.py
+++ link_finder.py
@@ -6,19 +6,26 @@ from urllib import parse
 
 class LinkFinder(HTMLParser):
 
-    def __init__(self):
+    def __init__(self, base_url, page_url):
         super().__init__()
+        self.base_url = base_url
+        self.page_url = page_url
+        self.links = set()
 
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
             for (attribute, value) in attrs:
                 if attribute == 'href':
-                    url = parse.urljoin()
+                    url = parse.urljoin(self.base_url, value)
+                    self.links.add(url)
+
+    def page_links(self):
+        return self.links
 
     def error(self, message):
         pass
 
-
-finder = LinkFinder()
-finder.feed('<html><head><title>Test</title></head>'
-            '<body><h1>Parse me!</h1></body></html>')
+#
+# finder = LinkFinder()
+# finder.feed('<html><head><title>Test</title></head>'
+#             '<body><h1>Parse me!</h1></body></html>')
