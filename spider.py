64622242fad030c0b329e8bd0c064142ec4f1b55
Charles De Barros <charlesdebarros@yahoo.co.uk>
2016-07-24 18:14:36 +0100

    Adding 'staticmethod 'craw_page.

--- spider.py
+++ spider.py
@@ -31,3 +31,14 @@ class Spider:
         create_data_files(Spider.project_name, Spider.base_url)
         Spider.queue = file_to_set(Spider.queue_file)
         Spider.crawled = file_to_set(Spider.crawled_file)
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + ' now crawling ' + page_url)
+            print('Queue ' + str(leng(Spider.queue)) +
+                  ' | Crawled ' + str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_links(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()
